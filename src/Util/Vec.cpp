9cdab5bdcc9e8323de7bbc32c8fbe5d2ff9faad2
jackwthake <jackthake@hotmail.com>
2024-11-29 23:48:51 -0800

    Added simple vector class

---
+++ src/Util/Vec.cpp
@@ -0,0 +1,69 @@
+#include "Vec.h"
+
+#include <cmath>
+
+static double angleBetweenVectors(Vector2& A, const Vector2& B) {
+    // Compute dot product and magnitudes
+    double dot = A * B;
+    double magA = A.mag();
+    double magB = B.mag();
+
+    // Avoid division by zero and clamp the cosine value to the range [-1, 1]
+    double cosTheta = dot / (magA * magB);
+    cosTheta = std::fmin(1.0, std::fmax(-1.0, cosTheta));  // Clamp to [-1, 1]
+
+    // Return the angle in radians
+    return acos(cosTheta);
+}
+
+Vector2::Vector2(void) : Vector2(0, 0) {}
+Vector2::Vector2(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+Vector2::Vector2(const Vector2 &rhs) {
+  this->x = rhs.x;
+  this->y = rhs.y;
+}
+
+void Vector2::operator=(const Vector2 &rhs) {
+  this->x = rhs.x;
+  this->y = rhs.y;
+}
+
+Vector2 Vector2::operator+(const Vector2 &rhs) {
+  return Vector2(this->x + rhs.x, this->y + rhs.y);
+}
+
+Vector2 Vector2::operator-(const Vector2 &rhs) {
+  return Vector2(this->x - rhs.x, this->y - rhs.y);
+}
+
+void Vector2::operator*=(const double scaler) {
+  this->x *= scaler;
+  this->y *= scaler;
+}
+
+void Vector2::operator/=(const double scaler) {
+  this->x /= scaler;
+  this->y /= scaler;
+}
+
+double Vector2::operator*(const Vector2 &rhs) { // dot product
+  return (this->x * rhs.x) + (this->y * rhs.y);
+}
+
+double Vector2::cross_prod(const Vector2 &rhs) { // TODO: Fix;
+  return this->mag() * rhs.mag() * sin(angleBetweenVectors(*this, rhs));
+}
+
+double Vector2::mag() const {
+  return sqrt(pow(this->x, 2) + pow(this->y, 2));
+}
+
+std::ostream &operator<<(std::ostream &out, const Vector2 &vec) {
+  out << "< x: " << vec.x << " y: " << vec.y << " >";
+
+  return out;
+}
---
+++ src/Util/Vec.h
@@ -0,0 +1,30 @@
+#ifndef VEC_H
+#define VEC_H
+
+#include <ostream>
+
+class Vector2 {
+public:
+  Vector2(void);
+  Vector2(double x, double y);
+  Vector2(const Vector2 &rhs);
+
+  void operator=(const Vector2 &rhs);
+
+  Vector2 operator+(const Vector2 &Rhs);
+  Vector2 operator-(const Vector2 &Rhs);
+
+  void operator*=(const double scaler);
+  void operator/=(const double scaler);
+
+  double operator*(const Vector2 &rhs); // dot product
+  double cross_prod(const Vector2 &rhs);
+  double mag() const;
+
+  friend std::ostream &operator<<(std::ostream &out, const Vector2 &);
+
+  double x, y;
+};
+
+
+#endif
