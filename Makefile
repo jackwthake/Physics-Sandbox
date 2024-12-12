d5c8d5b1ae2544426e4898a2eff5b1204cedd603
jackwthake <jackthake@hotmail.com>
2024-12-11 22:50:39 -0800

    Refactored main for gameobject system

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CFLAGS=-Wall -ansi -std=c++20
 LFLAGS=-lSDL2
 DFLAGS=-g
 PROGS=prog
-OBJS=vec.o
+OBJS=vec.o obj.o help.o
 
 all: $(OBJS) $(PROGS)
 
@@ -11,6 +11,12 @@ prog: src/main.cpp $(OBJS)
 	$(CC) $(CFLAGS) $(DFLAGS) $< -o $@ $(OBJS) $(LFLAGS)
 
 vec.o: src/Util/Vec.cpp src/Util/Vec.h
+	$(CC) $(CFLAGS) -c $(DFLAGS) $< -o $@
+
+obj.o: src/Object.cpp src/Object.h
+	$(CC) $(CFLAGS) -c $(DFLAGS) $< -o $@
+
+help.o: src/Util/Helpers.cpp src/Util/Helpers.h
 	$(CC) $(CFLAGS) -c $(DFLAGS) $< -o $@
 
 clean:
--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,9 @@
 
 #include <SDL2/SDL.h>
 #include "Util/Vec.h"
+#include "Util/Helpers.h"
 
-const int WIDTH = 640, HEIGHT = 480;
-const int TICKS_PER_SECOND = 5;
-const int TICK_INTERVAL = 1000 / TICKS_PER_SECOND;  // milliseconds per tick
-
-/* Physics Constants */
-const double G = 9.81;
-const double Pixel_To_Meter = 32; // 1 meter = 32 pixels
-const int Floor_Height = 15; // 15 meters from top left origin
-
-/* Basic physics object, has poisition, bounds, and something to modulate position. */
-struct Object {
-  Vector2 position = { 4, 0 }, velocity = { 0, 0 }, acceleration = { 0, 0 };
-  Vector2 bounds = { 1, 1 }; // 1 meter x 1 meter
-
-  double mass = 1; // 1kg
-  bool grounded = false;
-};
-
-
-/* Helper function to set one pixel's color in the framebuffer */
-static void set_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel) {
-  Uint32 * const target_pixel = (Uint32 *) ((Uint8 *) surface->pixels
-                                                    + y * surface->pitch
-                                                    + x * surface->format->BytesPerPixel);
-  *target_pixel = pixel;
-}
-
-
-/* Helper function to draw rect at specified x, y of w, h */
-static void draw_rect(SDL_Surface *surf, Vector2 &pos, Vector2 &bounds, Uint32 pixel) {
-    int i, j;
-    for (i = pos.y * Pixel_To_Meter; i < pos.y * Pixel_To_Meter + bounds.y * Pixel_To_Meter; ++i) {
-        for (j = pos.x * Pixel_To_Meter; j < pos.x * Pixel_To_Meter + bounds.x * Pixel_To_Meter; ++j) {
-            if (i <= HEIGHT && j <= WIDTH && i >= 0 && j >= 0)
-              set_pixel(surf, j, i, pixel);
-        }
-    }
-}
-
+#include "Object.h"
 
 /* Helper. polls window for new input  */
 static int window_poll_event(void) {
@@ -67,59 +30,22 @@ static void swap_surface(SDL_Surface *x, SDL_Surface *y) {
     y = temp;
 }
 
-/* Apply a force to an objects acceleration vetor */
-void apply_force(Object &obj, Vector2 &F) {
-    obj.acceleration += F;
-}
 
-/* Update Object */
-void update_object(Object &obj, double dt) {
-  if (obj.position.y + obj.bounds.y < Floor_Height)
-    obj.acceleration.y = obj.mass * G * dt;
-
-  if (obj.position.y + obj.bounds.y >= Floor_Height) {
-    obj.acceleration.y = 0;
-    obj.velocity.y = 0;
-    obj.position.y = Floor_Height - obj.bounds.y;
-
-    obj.grounded = true;
-  }
-
-  const uint8_t *key_state = SDL_GetKeyboardState(NULL);
-  if (key_state[SDL_SCANCODE_SPACE] && obj.grounded) {
-    Vector2 F = { 0.0, -5 * obj.mass * G * dt }; // Apply an upward force to the object
-    apply_force(obj, F);
-
-    obj.grounded = false;
-  }
-
-  /* update position */
-  obj.velocity += obj.acceleration;
-  obj.position += obj.velocity;
-
-  std::cout << "position: " << obj.position << "\nvelocity: " << obj.velocity << "\nacceleration: " << obj.acceleration << std::endl;
-}
-
-
-void render_object(SDL_Surface *surf, Object &obj) {
-  draw_rect(surf, obj.position, obj.bounds, 0xFFFFFF);
-}
-
+/* Main gameloop, maintains a semi-fixed timestep */
 void main_loop(SDL_Window *window, SDL_Surface *surf, SDL_Surface *back) {
   using namespace std;
   using namespace chrono;
 
+  /* Setup clocks */
   steady_clock::time_point lastTime = steady_clock::now();
   steady_clock::time_point currentTime;
-
-  Object obj;
 
   while (!window_poll_event()) {
     currentTime = steady_clock::now();
     auto deltaTime = currentTime - lastTime;
 
     if (deltaTime.count() >= TICK_INTERVAL) {
-      update_object(obj, duration<double>(deltaTime).count());
+      Object::update_objects(duration<double>(deltaTime).count());
 
       lastTime = currentTime;  // Reset the last time
     }
@@ -128,7 +54,7 @@ void main_loop(SDL_Window *window, SDL_Surface *surf, SDL_Surface *back) {
     memset(surf->pixels, 0x00, surf->w * surf->h * surf->format->BytesPerPixel);
 
     /* draw game */
-    render_object(surf, obj);
+    Object::render_objects(surf);
 
     /* render framebuffer and swap buffers */
     SDL_UpdateWindowSurface(window);
